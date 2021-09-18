bfab9946c2c57cc2b74c3a5f39075839e44351aa
var <varqiao@qq.com>
2021-09-18 17:07:03 +0800

    增加service

---
+++ src/main/java/com/var/mock/service/mockService.java
@@ -0,0 +1,32 @@
+package com.var.mock.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class mockService {
+
+//    将路径和对应的json存储到redis里面
+    public Integer saveToRedis(String path, String json){
+
+//        to do save to redis
+        return 0;
+    }
+
+
+//    判断是否有相应的路径
+    public Boolean isExist(String path){
+
+//        to do 判断redis里面是否有这个路径
+
+        return false;
+    }
+
+//    通过路径获取相对应的json
+    public String getJsonByPath(String path){
+
+//        to do
+
+        return null;
+    }
+
+}
